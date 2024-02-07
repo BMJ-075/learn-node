90f2db576172b0e4f23a51309fbb15072f8f3d86
bhavya <bhavya@mindpathtech.com>
2024-02-07 09:50:07 +0530

    require and exports

--- hello.js
+++ hello.js
@@ -1,1 +1,27 @@
 console.log('Welcome to the Node js');
+
+
+function add(){
+    return 1+2
+}
+
+
+
+
+class Car {
+    seats;
+
+    constructor( seats ){
+        this.seats = seats;
+    }
+
+    start() {
+
+    }
+    changGear() {
+
+    }
+}
+
+const myCar = new Car(4);
+const AmansCar = new Car(6);
---
+++ notes-app/utils.js
@@ -0,0 +1,6 @@
+console.log("other file");
+
+const name = "bhavya";
+
+
+module.exports = name;
